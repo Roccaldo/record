6308bce04086ab4f37e4e596da4708687a41ebb6
Roccaldo <roccaldodigrisolo@gmail.com>
2023-12-09 14:28:25 +0100

    add esercizio

---
+++ src/Main.java
@@ -0,0 +1,8 @@
+public class Main {
+    public static void main(String[] args) {
+        //crea Punto
+        Punto punto = new Punto(20, 67);
+        //stampa punto
+        System.out.println(punto);
+    }
+}
